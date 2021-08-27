8f42efb7921e694adf2a2354c3b220c9e4d5dbc0
EVELIIiiNA <evelina.bobina@mail.ru>
2021-08-27 20:41:09 +0300

    exam2

---
+++ ekz3.py.py
@@ -0,0 +1,20 @@
+# def creditcart():
+#     number=input("введите номер карты:")
+#     numbers=["*"*12+number[-4]+number[-3]+number[-2]+number[-1]]
+#     print(numbers)
+#
+#
+# creditcart()
+
+# def palind():
+#     slv=input("введите слово")
+#     if slv == slv[::-1]:
+#         print("слово палиндром")
+#     else:
+#         print("нет")
+#
+#
+#
+# palind()
+
+
